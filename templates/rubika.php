f3bbe1d0efe19552b8af9063edc83b5e683b46fb
Xavier MMI <xavier.senente@univ-fcomte.fr>
2022-03-09 18:43:54 +0100

    facebook pixel

--- templates/rubika.php
+++ templates/rubika.php
@@ -46,6 +46,46 @@ Template Name: Concours Rubika
       display: none;
     }
   </style>
+
+  <!-- Facebook Pixel Code Numerica -->
+  <script>
+    !function(f,b,e,v,n,t,s)
+    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+    n.queue=[];t=b.createElement(e);t.async=!0;
+    t.src=v;s=b.getElementsByTagName(e)[0];
+    s.parentNode.insertBefore(t,s)}(window, document,'script',
+    'https://connect.facebook.net/en_US/fbevents.js');
+    fbq('init', '323602496364373');
+    fbq('track', 'PageView');
+
+    window.onload = function(e) {
+        
+    function editForm() {
+      fbq('trackCustom', 'startcompletion');
+    }
+
+    function sendForm() {
+      fbq('track', 'CompleteRegistration');
+    }
+
+    document.querySelector("#input_4_10_2").addEventListener("focus", editForm );
+    document.querySelector("#input_4_10_3").addEventListener("keypress", editForm );
+    document.querySelector("#input_4_10_6").addEventListener("keypress", editForm );
+    document.querySelector("#input_4_12").addEventListener("keypress", editForm );
+    document.querySelector("#input_4_11").addEventListener("keypress", editForm );
+    document.querySelector("#gform_submit_button_4").addEventListener("click", sendForm );
+
+  };
+
+      
+  </script>
+
+  <noscript><img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=323602496364373&ev=PageView&noscript=1"/>
+  </noscript>
+
+<!-- End Facebook Pixel Code Numerica -->
 </head>
 
 <body <?php body_class(); ?>>
